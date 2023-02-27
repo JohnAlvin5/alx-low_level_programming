687239ae6b73ed35b39f21c8e95852a15cf851dd
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-02-27 15:38:16 -0800

    Finished number 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,27 @@
 #include "main.h"
+#include <string.h>
 
 /**
- *  - writes the character c to stdout
- * The character to print
+ * puts2 - writes the character c to stdout
+ * @str : The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+void puts_half(char *str)
+{
+	int length, i, j;
+
+	length = strlen(str);
+
+	if ((length % 2) != 0)
+		j = (length + 1) / 2;
+	else
+		j = length /  2;
+
+	for (i = j; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
