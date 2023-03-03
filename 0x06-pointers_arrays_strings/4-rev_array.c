beb9c359335c0a4df0b0eaea81038dd86e50858c
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-03 10:14:13 -0800

    Final corrections

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,14 +12,14 @@ void reverse_array(int *a, int n)
 {
 	int temp;
 	int i;
-	
-	for(i = 0; i < (n / 2); i++)
+
+	for (i = 0; i < (n / 2); i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = temp;
 	}
 
-	for(i = 4; i < n; i++)
+	for (i = 4; i < n; i++)
 		_putchar(a[i]);
 }
