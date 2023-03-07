aab06fcffd910c33b0c61a4656d8dcd57c856662
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-07 06:02:13 -0800

    Initialisation

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *  - writes the character c to stdout
+ * @ : The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
