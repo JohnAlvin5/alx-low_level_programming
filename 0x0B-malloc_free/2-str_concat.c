b5a4ccc87e51181a36a0edcaa74bd47661bce0bd
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-14 12:35:04 -0700

    Question 0, 1, 2 correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,9 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1[0] = '\0';
 	if (s2 == NULL)
-		s2 = '\0';
+		s2[0] = '\0';
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
