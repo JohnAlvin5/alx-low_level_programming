7e7deeb482069a27907b126fbb55b1d3d1b4d2e7
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-16 13:44:36 -0700

    Question 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
-	str = malloc(sizeof(char) * (len1 + len2 + 1));
+	str = malloc(sizeof(char) * (len1 + len2));
 
 	if (str == NULL)
 		return (NULL);
