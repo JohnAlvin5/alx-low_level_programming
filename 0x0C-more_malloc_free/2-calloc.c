6df778d8cc57ec2df5cb613c923b5f3e345181b8
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-16 11:54:51 -0700

    Question 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 
 /**
- *  - writes the character c to stdout
- * @ : The character to print
- * @ : second
+ * calloc - allocates memory for an array
+ * @nmemb : number of elements
+ * @size : size in bytes
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+/*	unsigned int i;*/
+
+	unsigned int *str;
+
+	str = (unsigned int *)malloc(nmemb * size);
+
+	if (str == NULL || nmemb == 0 || size == 0)
+		return (NULL);
+
+/**	for (i = 0; i < nmemb; i++)
+ *	{
+ *		str[i] = 0;
+ *	}
+ */
+	return (str);
+	free(str);
+
+}
