63f9b691c0656dfd0407e29b65466b99ecf5abd7
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-16 11:18:43 -0700

    Question 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- *  - writes the character c to stdout
- * @ : The character to print
- * @ : second
+ * string_nconcat - concatenates n characters string s2 to s1
+ * @s1 :First string
+ * @s2 : second string
+ * @n : Number of characters to concatenate
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+	unsigned int len1, len2;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j <= len2 && j <= n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+
+	return (str);
+
+	free(str);
+}
