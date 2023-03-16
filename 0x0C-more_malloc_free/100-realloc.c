6b7de4c59d71f60a51d12e7fde76978d97c69916
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-16 13:51:37 -0700

    Question 3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,7 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < new_size; i++)
 	{
-		new[i] = ptr[i];
+		new[i] = *((int *)(ptr) + i);
 	}
 
 	return (new);
