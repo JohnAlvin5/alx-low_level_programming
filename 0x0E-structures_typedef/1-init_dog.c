526a0f14df513d9167528e35e7fadec537adc02a
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-21 06:44:58 -0700

    Second deadline corrections

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initialises a variable of struct dog
@@ -19,7 +20,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (p == NULL)
 	{
 		printf("Ok");
-		return;
+		exit(0);
 	}
 
 	p->name = name;
