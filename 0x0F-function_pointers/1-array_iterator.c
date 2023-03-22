114a29394770bef865eeddea36d8250e2e44fafb
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-22 13:08:30 -0700

    Progress to question 3

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
+	if (!array || !size || !action)
+		return;
+
 	for (x = 0; x < size; x++)
 	{
 		action(array[x]);
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if(size <= 0)
+	if (!array || !size || !cmp)
+		return (-1);
+
+	if (size <= 0)
 		return (-1);
 
 	for (x = 0; x <= size; x++)
