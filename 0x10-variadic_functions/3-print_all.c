1b15a3b75b328ffad8ea2b7569f65899031df413
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-03-23 14:06:01 -0700

    Starting #3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "varadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format - list of arguements passed to function
+ *
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+
