0b05b26e1772eb2ba1c6871f0a1e1ea31e19bd82
Alvin Wanangwe <WanangweAlvin@gmail.com>
2023-04-10 12:03:09 -0700

    Task #2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,3 +9,27 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fp, write_no, i;
+
+	if (!filename)
+		return (-1);
+
+	fp = open(filename, O_WRONLY | O_APPEND);
+
+	if (fp == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+
+		write_no = write(fp, text_content, i);
+
+		if (write_no == -1)
+			return (-1);
+	}
+
+	close(fp);
+	return (1);
+}
